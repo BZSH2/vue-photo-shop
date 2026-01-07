7f98a300dff1d67b21bd8723b439c1925a7b8b75
wangbo <99548050@qq.com>
2026-01-07 15:41:35 +0800

    feat: 🎸 初始化nav部分

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="layout-container">
-    <Header />
+    <header class="header-inner">
+      <slot name="header" />
+    </header>
     <div class="layout-main">
-      <Nav />
+      <slot name="nav" />
       <Main />
       <Aside />
     </div>
@@ -11,14 +13,20 @@
 
 <script setup lang="ts">
 import Aside from "./components/Aside.vue";
-import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
-import Nav from "./components/Nav.vue";
 </script>
 
 <style lang="scss" scoped>
 .layout-container {
   background-color: var(--background-color-secondary-regular);
+
+  .header-inner {
+    height: 60px;
+    white-space: nowrap;
+    background-color: var(--background-color-primary-regular);
+    border-bottom: 1px solid var(--background-color-divider-regular);
+  }
+
   .layout-main {
     height: calc(100% - 60px);
     display: flex;
---
+++ src/views/Nav/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="nav-panel">
+    <NavAside :active="active" />
+    <div class="nav-inner">
+      222
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import NavAside from "./components/NavAside.vue";
+
+const active = ref("add");
+</script>
+
+<style lang="scss" scoped>
+.nav-panel {
+  height: 100%;
+  display: flex;
+}
+
+.nav-inner {
+  height: 100%;
+  width: 200px;
+  background-color: var(--background-color-primary-regular);
+  border-right: 1px solid var(--background-color-divider-regular);
+}
+</style>
