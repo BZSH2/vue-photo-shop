8e762608099bc72a4157ebb090c7ec196768bdf5
wangbo <99548050@qq.com>
2026-01-22 17:31:55 +0800

    test: 💍 rul

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import AutoImport from "unplugin-auto-import/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import Components from "unplugin-vue-components/vite";
-import { defineConfig } from "vite";
-import vueDevTools from "vite-plugin-vue-devtools";
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // 将根路径换成相对路径
+  base: './', // 将根路径换成相对路径
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
     AutoImport({
-      imports: ["vue", "vue-router"],
+      imports: ['vue', 'vue-router'],
       resolvers: [ElementPlusResolver()],
     }),
     Components({
@@ -25,7 +25,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
