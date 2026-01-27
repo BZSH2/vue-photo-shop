a9a0de11b1b1592012444550b61a53843035dd44
wangbo <99548050@qq.com>
2026-01-27 10:11:20 +0800

    test: 💍 build

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,5 +1,8 @@
 // src/utils/path.js
 export function getPublicPath(): string {
-  const base = import.meta.env.BASE_URL;
-  return base === '/' ? '' : base.replace(/\/$/, '');
+  // 如果是 GitHub Pages 环境
+  if (window.location.host.includes('github.io')) {
+    return '/vue-photo-shop'; // 替换为你的仓库名
+  }
+  return ''; // 开发环境
 }
